5e3b0e9f0194bf1a3653eaf5bbe1621827bcb986
rdosrun <rdosrun@gmail.com>
2025-03-03 12:35:59 -0700

    init

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './style/style.css';
-import './style/headerbar.css';
-import './style/qr_style.css';
-import './style/aboutus.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
