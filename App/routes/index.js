d59b7f8be43e6b1a30484b79f94f70a797f77945
rdosrun <rdosrun@gmail.com>
2025-03-05 10:48:04 -0700

    Log complete request object in POST route for debugging

--- App/routes/index.js
+++ App/routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-    console.log(req.client_info);
+    console.log("test",req);
     res.render('index', {
         title: 'MSAL Node & Express Web App',
         isAuthenticated: req.session.isAuthenticated,
