57f5ca77c62180c1f9d926bf709f096cf68482be
rdosrun <rdosrun@gmail.com>
2025-03-08 16:58:20 -0700

    Refactor get_token to use jsrsasign library for JWT creation and update environment variable access

--- App/routes/libs/get_token.js
+++ App/routes/libs/get_token.js
@@ -2,6 +2,7 @@ var https = require('follow-redirects').https;
 var fs = require('fs');
 var qs = require('querystring');
 var path = require('path');
+var jsrsasign = require('jsrsasign');
 
 // Load environment variables from the Postman environment file
 const envFilePath = path.join(__dirname, '../../Arcadian Outfitters LLC.postman_environment.json');
@@ -13,7 +14,7 @@ function get_token() {
     return new Promise((resolve, reject) => {
         var navigator = {}; // necessary as part of "eval" on jsrsasign lib
         var window = {}; // necessary as part of "eval" on jsrsasign lib
-        eval(pm.globals.get("jsrsasign-js")); // grabbing jsrsasign lib, loaded in separate GET 
+        //eval(pm.globals.get("jsrsasign-js")); // grabbing jsrsasign lib, loaded in separate GET 
 
         const cryptojs = require('crypto-js'); // using crypto js for base64 encoding
 
@@ -28,7 +29,7 @@ function get_token() {
 
         // Create JWT payload
         let jwtPayload = {
-            iss: pm.environment.get(CONSUMER_KEY), // consumer key of integration record
+            iss: CONSUMER_KEY, // consumer key of integration record
             scope: ['restlets','rest_webservices', 'suite_analytics'], // scopes specified on integration record
             iat: (new Date() / 1000),               // timestamp in seconds
             exp: (new Date() / 1000) + 3600,        // timestamp in seconds, 1 hour later, which is max for expiration
@@ -38,7 +39,7 @@ function get_token() {
         var stringifiedJwtPayload = JSON.stringify(jwtPayload);
 
         // The secret is the private key of the certificate loaded into the client credentials mapping in NetSuite
-        let secret = pm.environment.get(CERTIFICATE_PRIVATE_KEY);
+        let secret = CERTIFICATE_PRIVATE_KEY;
         let encodedSecret = cryptojs.enc.Base64.stringify(cryptojs.enc.Utf8.parse(secret)); // we need to base64 encode the key
 
         // Sign the JWT with the PS256 algorithm (algorithm must match what is specified in JWT header).
@@ -46,8 +47,8 @@ function get_token() {
         let signedJWT = KJUR.jws.JWS.sign('PS256',stringifiedJwtHeader,stringifiedJwtPayload,secret);
 
         // The signed JWT is the client assertion (encoded JWT) that is used to retrieve an access token
-        pm.collectionVariables.set('clientAssertion', signedJWT);
-
+        //.collectionVariables.set('clientAssertion', signedJWT);
+        return signedJWT;
         
     });
 }
