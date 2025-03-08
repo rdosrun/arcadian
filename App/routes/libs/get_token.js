3df3bcfc5e5b8e60c32b34598ddc5175d459f2ad
rdosrun <rdosrun@gmail.com>
2025-03-08 12:45:23 -0700

    Update get_token function to return access token and await its retrieval in auth callback

--- App/routes/libs/get_token.js
+++ App/routes/libs/get_token.js
@@ -26,7 +26,7 @@ var req = https.request(options, function (res) {
     try {
         body = JSON.parse(body.toString());
         console.log("line28",body);
-        return body;
+        return body.access_token;
     }catch (error) {
         console.error("Error parsing JSON:", error);
         return null;
