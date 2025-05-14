fd91ac759014a490bc3deec644012620663d3be2
rdosrun <rdosrun@gmail.com>
2025-05-14 14:31:55 -0600

    Update upload route to use GET method and integrate with multer for file uploads

--- App/app.js
+++ App/app.js
@@ -58,10 +58,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
-//app.use('/upload', uploadPhotosRouter);
-app.use('/upload', function (req, res) {
-    res.status(200).json({ success: true, message: 'Upload route is working' });
-});
+app.use('/upload', uploadPhotosRouter);
 
 app.get('/views/:page', function (req, res, next) {
     const page = req.params.page + '.html';
