433420a9493cdd47750518360ae1a6b8bb283cf5
rdosrun <rdosrun@gmail.com>
2025-05-14 11:39:06 -0600

    Replace uploadPhotosRouter with a direct POST route for file uploads

--- App/app.js
+++ App/app.js
@@ -58,7 +58,10 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
-app.use('/upload', uploadPhotosRouter);
+//app.use('/upload', uploadPhotosRouter);
+app.post('/upload',function (req, res) {
+    return res.status(200).json({ success: true, message: 'File uploaded successfully' });
+});
 
 app.get('/views/:page', function (req, res, next) {
     const page = req.params.page + '.html';
