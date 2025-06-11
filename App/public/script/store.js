a721f8efa4a23d4cc208f111a308afa3431e5496
rdosrun <rdosrun@gmail.com>
2025-06-11 10:58:12 -0600

    Fix internal ID retrieval in place_order function to parse inventory from localStorage

--- App/public/script/store.js
+++ App/public/script/store.js
@@ -292,7 +292,7 @@ async function place_order(customer) {
             cart: cart.map(item => ({
                 ID: item.ID,
                 name: item.ID,
-                interal_ID: localStorage.getItem('inventory').find(i => (i.upc === item.ID && i.isinactive =='F'))?.internal_id || '',
+                interal_ID: JSON.parse(localStorage.getItem('inventory')).find(i => (i.upc === item.ID && i.isinactive =='F'))?.internal_id || '',
             })),
             customer: {
                 customer_internal_id: customer.customer_internal_id,
