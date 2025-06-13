27b31ba2ae9613ac4704cd062622b05c59633df9
rdosrun <rdosrun@gmail.com>
2025-06-12 20:00:33 -0600

    Update authority in msalConfig to use consumer endpoint for authentication

--- App/public/script/script.js
+++ App/public/script/script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const msalConfig = {
     auth: {
       clientId: '75c7b466-6b7e-4815-bf6b-8627cb7d1db8',//'95e880e8-e54d-4d01-a26c-052cff7e9592', // Replace with your application ID
-      authority: 'https://login.microsoftonline.com/6ba30675-1170-422f-89c3-0a43af4a4534', // Replace with your tenant ID
+      authority: 'https://login.microsoftonline.com/consumers', // Replace with your tenant ID
       redirectUri: window.location.origin
     }
   };
