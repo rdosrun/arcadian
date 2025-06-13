f86f65f8241673cf52c27e46fda52fd3336a9a49
rdosrun <rdosrun@gmail.com>
2025-06-13 13:47:58 -0600

    Update navigation to include Credit Memos and remove Re-Order option for authenticated users

--- App/app.js
+++ App/app.js
@@ -255,6 +255,47 @@ app.post('/submit-order', async (req, res) => {
     }
 });
 
+
+// Add endpoint for sales_order_lines
+app.get('/api/sales-order-lines/:id', async (req, res) => {
+    const salesOrderId = req.params.id;
+    try {
+        const data = await netsuite.sales_order_lines(salesOrderId);
+        res.json({ success: true, data: JSON.parse(data) });
+    } catch (error) {
+        console.error('Error fetching sales order lines:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
+app.get('/api/Credit-Memo-lines/:id', async (req, res) => {
+    const salesOrderId = req.params.id;
+    try {
+        const data = await netsuite.Credit_Memo_lines(salesOrderId);
+        res.json({ success: true, data: JSON.parse(data) });
+    } catch (error) {
+        console.error('Error fetching sales order lines:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
+app.get('/api/Invoices/:id', async (req, res) => {
+    const salesOrderId = req.params.id;
+    try {
+        const data = await netsuite.Invoice_Lines(salesOrderId);
+        res.json({ success: true, data: JSON.parse(data) });
+    } catch (error) {
+        console.error('Error fetching sales order lines:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
+
+
+
+
+
+
 // Run get_token every hour
 setInterval(() => {
     try {
