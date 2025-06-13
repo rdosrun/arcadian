ea49d25a8c279474d17e6dafcb383604f0243153
rdosrun <rdosrun@gmail.com>
2025-06-12 19:16:43 -0600

    Update clientId in msalConfig to the correct application ID

--- App/public/script/script.js
+++ App/public/script/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   const msalConfig = {
     auth: {
-      clientId: '95e880e8-e54d-4d01-a26c-052cff7e9592', // Replace with your application ID
+      clientId: '75c7b466-6b7e-4815-bf6b-8627cb7d1db8',//'95e880e8-e54d-4d01-a26c-052cff7e9592', // Replace with your application ID
       authority: 'https://login.microsoftonline.com/6ba30675-1170-422f-89c3-0a43af4a4534', // Replace with your tenant ID
       redirectUri: window.location.origin
     }
