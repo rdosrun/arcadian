7c923d058b617748f596fa718d01d19aad3a771c
rdosrun <rdosrun@gmail.com>
2025-06-20 18:54:54 -0600

    Update environment variables for MSAL configuration and correct redirect URIs

--- App/public/script/script.js
+++ App/public/script/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   const msalConfig = {
     auth: {
-      clientId: '75c7b466-6b7e-4815-bf6b-8627cb7d1db8',//'95e880e8-e54d-4d01-a26c-052cff7e9592', // Replace with your application ID
+      clientId: '8b1fadc1-8d5e-4543-a2d2-27675cdd49e2',//'95e880e8-e54d-4d01-a26c-052cff7e9592', // Replace with your application ID
       authority: 'https://login.microsoftonline.com/consumers', // Replace with your tenant ID
       redirectUri: 'https://arcadianoutfitters.com/test/auth/callback/'
     }
