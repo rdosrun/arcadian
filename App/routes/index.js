33ea4477db2981600a194d530fbe786f7dac9d64
rdosrun <rdosrun@gmail.com>
2025-06-22 12:10:29 -0600

    Log request body in JWT route for debugging

--- App/routes/index.js
+++ App/routes/index.js
@@ -60,6 +60,7 @@ router.get('/auth/callback/views/script.js', function (req, res) {
 });
 
 router.post('/auth/jwt_route', async function (req,res,next){
+    console.log("JWT Route Loaded", req.body);
     token = JSON.parse(req.body);
     console.log("JWT Route Loaded", token);
 
