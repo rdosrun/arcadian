58154292c4b2faaec14bc043fbc8ac1cf5541b14
rdosrun <rdosrun@gmail.com>
2025-07-06 12:48:38 -0600

    Remove debug logging for request URL in middleware

--- App/app.js
+++ App/app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
   if (req.url.startsWith('/test')) {
     req.url = req.url.slice(5) || '/'; // remove `/test`, preserve slash
   }
-  console.log("Request URL: " + req.url);
+  //console.log("Request URL: " + req.url);
   next();
 });
 
